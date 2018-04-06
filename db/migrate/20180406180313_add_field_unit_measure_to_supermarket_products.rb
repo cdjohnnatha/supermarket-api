666386100d6728c6536cd8d7de0bce84d88681c0
cdjohnnatha <cdjohnnatha@gmail.com>
2018-04-06 15:44:27 -0300

    reload migration

--- db/migrate/20180406180313_add_field_unit_measure_to_supermarket_products.rb
+++ db/migrate/20180406180313_add_field_unit_measure_to_supermarket_products.rb
@@ -1,5 +1,5 @@
 class AddFieldUnitMeasureToSupermarketProducts < ActiveRecord::Migration[5.1]
   def change
-    add_column :supermarket_products, :unit_measure, :string, limit: 15
+    add_column :supermarket_products, :unit_measure, :string
   end
 end
