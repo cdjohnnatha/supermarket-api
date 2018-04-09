5601ee9cefd49e0d55c26458c8c2df8499efa695
cdjohnnatha <cdjohnnatha@gmail.com>
2018-04-09 00:02:45 -0300

    create routing for supermarket products

---
+++ spec/routing/supermarket_product_routing_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Api::V1::SupermarketProductsController, type: :routing do
+
+  describe "routing" do
+
+    it "routes to #index" do
+      expect(get: "/v1/supermarkets/1/products").to route_to("api/v1/supermarket_products#index")
+    end
+
+    it "routes to #show" do
+      expect(get: "/v1/supermarkets/1/products/1").to route_to("api/v1/supermarket_products#show", supermarket_id: "1", id: "1")
+    end
+
+    it "routes to #create" do
+     expect(post: "/v1/supermarkets/1/products").to route_to("api/v1/supermarket_products#create")
+   end
+
+    it "routes to #update via PUT" do
+      expect(put: "/v1/supermarkets/1/products/1").to route_to("api/v1/supermarket_products#update", supermarket_id: "1", id: "1")
+    end
+
+    it "routes to #update via PATCH" do
+      expect(patch: "/v1/supermarkets/1/products/1").to route_to("api/v1/supermarket_products#update", supermarket_id: "1", id: "1")
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "/v1/supermarkets/1/products/1").to route_to("api/v1/supermarket_products#destroy", supermarket_id: "1", id: "1")
+    end
+  end
+end
